0ed5af292d66d3b71e256aa87dff6bcaf38870bc
JustPyrrha <pyrrha.social@gmail.com>
2023-10-04 00:09:49 +0000

    Deploying to gh-pages from @ JustPyrrha/pyrrha.dev@cdcc7e70822f87503898b3145bd1c103735cf603 🚀

--- pyrrha-dev-33d472380cd6b18f.js
+++ pyrrha-dev-33d472380cd6b18f.js
@@ -651,7 +651,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('pyrrha-dev-e37dbb5aaf01813_bg.wasm', import.meta.url);
+        input = new URL('pyrrha-dev-33d472380cd6b18f_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
